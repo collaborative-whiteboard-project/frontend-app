68081631cfde3bb15f040a87e78c95b9cbcb7756
npolak <nikoem.polak@itsilesia.com>
2023-11-02 17:13:40 +0100

    Install PrimeNG components

--- collaborative-whiteboard/src/app/app.module.ts
+++ collaborative-whiteboard/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { EditorComponent } from './components/editor/editor.component';
 import { WhiteboardComponent } from './components/editor/whiteboard/whiteboard.component';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [AppComponent, EditorComponent, WhiteboardComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, ButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
