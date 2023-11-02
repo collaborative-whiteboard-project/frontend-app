97c4f9c3985af2e1c4c81ea8daa62a8dd1f5e495
npolak <nikoem.polak@itsilesia.com>
2023-11-02 16:48:04 +0100

    Init editor and whiteboard components

--- collaborative-whiteboard/src/app/app.module.ts
+++ collaborative-whiteboard/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EditorComponent } from './components/editor/editor.component';
+import { WhiteboardComponent } from './components/editor/whiteboard/whiteboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, EditorComponent, WhiteboardComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
