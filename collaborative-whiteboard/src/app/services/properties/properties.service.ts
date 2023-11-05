8b6c1bcb94ff2137b731ba322d837cca76a18fcd
npolak <nikoem.polak@itsilesia.com>
2023-11-05 22:32:38 +0100

    Create PropertiesService

--- collaborative-whiteboard/src/app/services/properties/properties.service.ts
+++ collaborative-whiteboard/src/app/services/properties/properties.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { SvgElementProperties } from 'src/app/shared/svg-element-properties.interface';
 
 @Injectable({ providedIn: 'root' })
-export class PropertiesService {}
+export class PropertiesService {
+  clearSelectedEventEmmiter = new Subject<void>();
+  updatePropertiesEventEmmiter = new Subject<SvgElementProperties>();
+  sendPropertiesEventEmmiter = new Subject<SvgElementProperties>();
+}
