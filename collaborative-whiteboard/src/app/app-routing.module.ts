31ba51193dceef20b3120b94cc382e2e491977d5
nikodem-pol <nw.polak@gmail.com>
2024-01-12 12:50:38 +0100

    Upadate routing

--- collaborative-whiteboard/src/app/app-routing.module.ts
+++ collaborative-whiteboard/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { AuthComponent } from './components/auth/auth.component';
 import { EditorComponent } from './pages/editor/editor.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const appRoutes: Routes = [
   { path: '', component: AuthComponent },
   { path: 'whiteboard/:id', component: EditorComponent },
-  // {path: "dashboard", component: DashboardComponent},
+  { path: 'dashboard', component: DashboardComponent },
   // {path: 'not-found', component: PageNotFoundComponent},
   // {path: '**',  redirectTo: '/not-found'}
 ];
